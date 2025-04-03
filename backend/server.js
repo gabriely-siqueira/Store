5e77b53b5da5299be924ac862ae0bdc9f0a6499d
Gabriely Siqueira <gabrielysiqueira2004@gmail.com>
2025-04-03 20:54:41 -0300

    Zustand Store & Create Page Implementation

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from './config/db.js';
-import productRoutes from './routes/product.route.js';
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); // Opcional, mas útil para formulários
+
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.use("/api/products", productRoutes);
 
-// Middleware para permitir o uso de JSON no req.body
-app.use(express.json());
 app.listen(PORT, () => {
-    connectDB();
-    console.log('Server started at http://localhost:' + PORT);
+  console.log("🚀 Server started at http://localhost:" + PORT);
 });
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
-import { createRoot } from 'react-dom/client' 
-import './index.css'
-import App from './App.jsx'
-import * as React from 'react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>
-</React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<ChakraProvider>
+				<App />
+			</ChakraProvider>
+		</BrowserRouter>
+	</React.StrictMode>
+);
