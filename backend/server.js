bc4b41940d723cf3e191cae2a98b228f73a67030
Gabriely Siqueira <gabrielysiqueira2004@gmail.com>
2025-04-03 16:09:50 -0300

    building API

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
+import productRoutes from './routes/product.route.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.use("/api/products", productRoutes);
 
+// Middleware para permitir o uso de JSON no req.body
+app.use(express.json());
 app.listen(5000, () => {
     connectDB();
     console.log('Server started at http://localhost:5000');
